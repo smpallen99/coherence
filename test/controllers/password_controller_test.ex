f64c6167a334ed5d299566bf3449597bbafd027f
Long Nguyen <long.polyglot@gmail.com>
2016-10-18 23:33:32 +0700

    Add test for edit

--- test/controllers/password_controller_test.ex
+++ test/controllers/password_controller_test.ex
@@ -1,6 +1,7 @@
 defmodule CoherenceTest.PasswordController do
   use TestCoherence.ConnCase
   import TestCoherence.Router.Helpers
+  import Coherence.ControllerHelpers, only: [random_string: 1]
 
   setup %{conn: conn} do
     Application.put_env :coherence, :opts, [:confirmable, :authenticatable, :recoverable,
@@ -22,6 +23,34 @@ defmodule CoherenceTest.PasswordController do
       conn = post conn, password_path(conn, :create), params
       assert conn.private[:phoenix_flash] == %{"info" => "Reset email sent. Check your email for a reset link."}
       assert html_response(conn, 302)
+    end
+  end
+
+  describe "edit" do
+    test "invalid reset password token", %{conn: conn, user: user} do
+      params = %{"id" => "123456789"}
+      conn = get conn, password_path(conn, :edit, user), params
+      assert conn.private[:phoenix_flash] == %{"error" => "Invalid reset token."}
+      assert html_response(conn, 302)
+    end
+
+    test "valid token has expired", %{conn: conn} do
+      token = random_string 48
+      {:ok, sent_at} = Ecto.DateTime.cast("2016-01-01 00:00:00")
+      insert_user(%{reset_password_sent_at: sent_at, reset_password_token: token})
+      params = %{"id" => token}
+      conn = get conn, password_path(conn, :edit, token), params
+      assert conn.private[:phoenix_flash] == %{"error" => "Password reset token expired."}
+      assert html_response(conn, 302)
+    end
+
+    test "valid token hasn't expired", %{conn: conn} do
+      token = random_string 48
+      insert_user(%{reset_password_sent_at: Ecto.DateTime.utc, reset_password_token: token})
+      params = %{"id" => token}
+      conn = get conn, password_path(conn, :edit, token), params
+      assert conn.private[:phoenix_template] == "edit.html"
+      assert html_response(conn, 200)
     end
   end
 end
