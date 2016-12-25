c216f2ce26610af02b7e315b86bdb82a9139be41
Buck Doyle <b@chromatin.ca>
2016-12-25 11:37:44 -0500

    Add UUID override to model parent

--- web/web.ex
+++ web/web.ex
@@ -7,6 +7,9 @@ defmodule Coherence.Web do
       import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
+
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
     end
   end
 
