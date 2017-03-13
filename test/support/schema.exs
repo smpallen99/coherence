ab1d204b23c4da595445b55a72050170b5404ce3
Stephen M. Pallen <smpallen99@yahoo.com>
2017-03-13 05:36:43 -0400

    fix tests to support updated Ecto

--- test/support/schema.exs
+++ test/support/schema.exs
@@ -11,13 +11,13 @@ defmodule TestCoherence.User do
     timestamps()
   end
 
-  @required_fields ~w(email name)
+  @required_fields ~w(email name)a
   @optional_fields ~w() ++ coherence_fields()
 
   def changeset(model, params \\ %{}) do
     model
-    |> cast(params, @required_fields, @optional_fields)
-    |> validate_required([:name, :email])
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint(:email)
     |> validate_coherence(params)
@@ -65,12 +65,13 @@ defmodule TestCoherence.Account do
     timestamps()
   end
 
-  @required_fields ~w(email name)
+  @required_fields ~w(email name)a
   @optional_fields ~w() ++ coherence_fields()
 
   def changeset(model, params \\ %{}) do
     model
-    |> cast(params, @required_fields, @optional_fields)
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
     |> unique_constraint(:email)
     |> validate_coherence(params)
   end
@@ -94,36 +95,12 @@ defmodule TestCoherence.Rememberable do
     timestamps()
   end
 
-  @required_fields ~w(series_hash token_hash token_created_at user_id)
+  @required_fields ~w(series_hash token_hash token_created_at user_id)a
   @optional_fields ~w()
 
   def changeset(model, params \\ %{}) do
     model
-    |> cast(params, @required_fields, @optional_fields)
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
   end
 end
-
-# defmodule TestCoherence.Trackable do
-#   use Coherence.Web, :model
-#   alias Coherence.Config
-
-#   schema "trackables" do
-#     field :action, :string, null: false
-#     field :sign_in_count, :integer, default: 0
-#     field :current_sign_in_at, Ecto.DateTime
-#     field :last_sign_in_at, Ecto.DateTime
-#     field :current_sign_in_ip, :string
-#     field :last_sign_in_ip, :string
-#     belongs_to :user, Module.concat(Config.module, Config.user_schema)
-
-#     timestamps()
-#   end
-
-#   @fields ~w(action sign_i_count current_sign_in_ip current_sign_in_at last_sign_in_ip last_sign_in_at user_id)a
-
-#   def changeset(model, params \\ %{}) do
-#     model
-#     |> cast(params, @fields)
-#     |> validate_required([:action, :user_id])
-#   end
-# end
