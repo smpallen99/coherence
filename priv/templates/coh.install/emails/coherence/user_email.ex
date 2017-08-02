bcff8bc6276596496e66da3376c8a8da2fe4c185
Stephen M. Pallen <smpallen99@yahoo.com>
2017-08-02 01:58:38 -0400

    fix email view namespace error

--- priv/templates/coh.install/emails/coherence/user_email.ex
+++ priv/templates/coh.install/emails/coherence/user_email.ex
@@ -2,7 +2,7 @@ Code.ensure_loaded Phoenix.Swoosh
 
 defmodule <%= web_base %>.Coherence.UserEmail do
   @moduledoc false
-  use Phoenix.Swoosh, view: Coherence.EmailView, layout: {<%= web_base %>.Coherence.LayoutView, :email}
+  use Phoenix.Swoosh, view: <%= web_base %>.Coherence.EmailView, layout: {<%= web_base %>.Coherence.LayoutView, :email}
   alias Swoosh.Email
   require Logger
   alias Coherence.Config
