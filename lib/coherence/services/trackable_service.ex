82bda398c4916dab14f9d1122a0fa5b4284494d0
peillis <enrique@doofinder.com>
2017-08-20 16:38:04 +0200

    fixing tests

--- lib/coherence/services/trackable_service.ex
+++ lib/coherence/services/trackable_service.ex
@@ -80,6 +80,7 @@ defmodule Coherence.TrackableService do
       {:ok, user} ->
         Config.auth_module
         |> apply(Config.update_login, [conn, user, [id_key: Config.schema_key]])
+        |> Conn.assign(Config.assigns_key, user)
       {:error, _changeset} ->
         Logger.error ("Failed to update tracking!")
         conn
