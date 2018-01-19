67032c6c7da9b6fc3bf2f62fe7528b3e2163cbb3
sashman <sashman90@gmail.com>
2018-01-19 17:07:15 +0000

    Fixing config url in controller

--- lib/coherence/controllers/controller.ex
+++ lib/coherence/controllers/controller.ex
@@ -161,7 +161,7 @@ defmodule Coherence.Controller do
   end
 
   def get_confirmation_url(conn, token) do
-    case Config.logged_in_url do
+    case Config.confimarion_url do
       nil -> router_helpers().confirmation_url(conn, :edit, token)
       url -> url
         |> String.replace(":token", token)
